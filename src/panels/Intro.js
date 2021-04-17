e974e5c3ffbdaed9394a44b8011734eabdb01bfa
YaDimaseks <iaremus.dk@phystech.edu>
2021-04-18 00:00:56 +0300

    Added Intro, but not tested

---
+++ src/panels/Intro.js
@@ -0,0 +1,44 @@
+import React, {Fragment} from 'react';
+
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+
+import './Intro.css';
+import Group from "@vkontakte/vkui/dist/components/Group/Group";
+import Div from "@vkontakte/vkui/dist/components/Div/Div";
+import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+
+
+const Intro = ({id, snackbarError, fetchedUser, userHasSeenIntro, go}) => {
+	return (
+        <Panel id={id} centered={true}>
+            <PanelHeader>
+                Cafe-app
+            </PanelHeader>
+            {(!userHasSeenIntro && fetchedUser) &&
+                <Fragment>
+                    <Group>
+                        <Div className='User'>
+                            {fetchedUser.photo_200 && <Avatar src={fetchedUser.photo_200}/>}
+                            <h2>Привет, {fetchedUser.first_name}!</h2>
+                            <h3>Этот сервис помогает купить заранее к определенному времени кофе или еду в ресторанах</h3>
+                        </Div>
+                    </Group>
+                    <FixedLayout vertical='bottom'>
+                        <Div className='OkButton'>
+                            <Button mode='commerce' size='xl' onClick={go}>
+                                OK, всё понятно!
+                            </Button>
+                        </Div>
+                    </FixedLayout>
+                </Fragment>
+
+            }
+            {snackbarError}
+        </Panel>
+    )
+};
+
+export default Intro;
